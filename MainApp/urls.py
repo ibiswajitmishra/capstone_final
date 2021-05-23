e72da95a0be0f050cea31c8264a01fc18b079ed8
ibiswajitmishra <biswajitmishrabikun@gmail.com>
2021-05-24 00:27:59 +0530

    mar

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.Home, name="home_page"),
     path('register/', views.Register, name="register_page"),
