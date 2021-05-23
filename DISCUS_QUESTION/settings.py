338e43c18a3e6f2a52b049ebf4f7fac3c26b7d56
ibiswajitmishra <biswajitmishrabikun@gmail.com>
2021-05-23 23:45:42 +0530

    rnd

--- DISCUS_QUESTION/settings.py
+++ DISCUS_QUESTION/settings.py
@@ -138,3 +138,7 @@ EMAIL_HOST_PASSWORD = params["EMAIL_HOST_PASSWORD"]
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
